33cf9838d5dfa19e3b0f25299e605c812a7a2a8c
leihaohao <2468048176@qq.com>
2025-03-19 17:38:15 +0800

    refactor(axios): 优化 Axios 插件实现
    - 重构 patchAxios 函数，使其更通用、更易用
    - 更新 createAxiosPlugin 函数，明确其使用方式
    - 调整类型定义，提高代码可读性和类型安全
    - 优化配置项合并逻辑，使用 mergeConfig 函数
    - 更新示例代码，使其更具指导性和可操作性

--- src/axios.ts
+++ src/axios.ts
@@ -1,44 +1,41 @@
 import type { ExecuteContext, InferTaskReturn, UseAsyncPlugin } from '@magic-js/use-async'
 import type { MaybeFn } from '@rhao/types-base'
-import type { AxiosInstance, AxiosRequestConfig, AxiosStatic } from 'axios'
-import axios from 'axios'
-import { merge, toValue } from 'nice-fns'
+import type { Axios, AxiosInstance, AxiosRequestConfig } from 'axios'
+import { mergeConfig } from 'axios'
+import { toValue } from 'nice-fns'
 
 interface PrivateStore {
   signal: AbortSignal
 }
 
-const PRIVATE_STORE_KEY = '_useAsyncAxiosPluginStore'
+const PRIVATE_STORE_KEY = '@@useAsyncAxiosPluginStore'
 
 const onceMap = new WeakMap()
 // 更改原型函数，执行任务时链接的上下文对象
 let executingCtx: ExecuteContext.Before | null = null
 
-patchAxios(axios.Axios)
-
 /**
- * 为 Axios 原型链上的 request 方法进行补丁，链接 useRequest 配置项，
- * 默认的 Axios 已被自动补丁，若是存在多 Axios 的场景下可手动调用进行补丁
+ * 为 Axios 原型链上的 request 方法进行补丁，链接 useRequest 配置项
  *
- * @param Axios Axios
+ * @param Constructor Axios
  *
  * @example
  * ```ts
- * import axios from 'axios'
+ * import { Axios } from 'axios'
  *
- * patchAxios(axios.Axios)
+ * patchAxios(Axios)
  * ```
  */
-export function patchAxios(Axios: typeof axios.Axios) {
-  if (onceMap.has(Axios)) {
+export function patchAxios(Constructor: typeof Axios) {
+  if (onceMap.has(Constructor)) {
     return
   }
-  onceMap.set(Axios, true)
+  onceMap.set(Constructor, true)
 
   // 原始的 `axios.request()`
-  const originalRequest = Axios.prototype.request
+  const originalRequest = Constructor.prototype.request
 
-  Axios.prototype.request = function request(
+  Constructor.prototype.request = function request(
     configOrUrl: string | AxiosRequestConfig,
     config?: AxiosRequestConfig,
   ) {
@@ -71,7 +68,7 @@ export function patchAxios(Axios: typeof axios.Axios) {
       let ctx = executingCtx
       executingCtx = null
 
-      config = merge({}, config, toValue(ctx.options.axiosConfig, this as AxiosInstance) || {})
+      config = mergeConfig(config, toValue(ctx.options.axiosConfig, this as AxiosInstance) || {})
       Object.assign(config, {
         [PRIVATE_STORE_KEY]: {
           signal: ctx.signal,
@@ -88,7 +85,21 @@ export function patchAxios(Axios: typeof axios.Axios) {
 
 /**
  * 创建 AxiosPlugin
- * @description 链接 `useAsync(task)` 和 `axios()`，仅支持链接 `task` 内部同步执行的 `axios()` 请求
+ *
+ * @description 链接 `useAsync(task)` 和 `axios()`，
+ * 仅支持链接 `task` 内部同步执行的 `axios()` 请求，需提前手动执行 `patchAxios(Axios)`。
+ *
+ * @example
+ * ```ts
+ * import { Axios } from 'axios'
+ *
+ * // 为 Axios 添加补丁
+ * patchAxios(Axios)
+ *
+ * createAsync({
+ *   plugins: [createAxiosPlugin()]
+ * })
+ * ```
  */
 export function createAxiosPlugin(): UseAsyncPlugin {
   return function AxiosPlugin(pluginCtx) {
@@ -107,9 +118,6 @@ declare module '@magic-js/use-async' {
     /**
      * axios 配置项
      */
-    axiosConfig?: MaybeFn<
-      AxiosRequestConfig<InferTaskReturn<T>>,
-      [axios: AxiosInstance | AxiosStatic]
-    >
+    axiosConfig?: MaybeFn<AxiosRequestConfig<InferTaskReturn<T>>, [axios: AxiosInstance]>
   }
 }
